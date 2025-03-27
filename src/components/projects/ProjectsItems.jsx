b6a8ef6ef36503e158610b8c60e2f829bd358c9e
Varun Jose <varunjose@dyn-169-226-241-2.wireless.albany.edu>
2025-03-26 21:36:31 -0400

    Updating projects section

--- src/components/projects/ProjectsItems.jsx
+++ src/components/projects/ProjectsItems.jsx
@@ -6,7 +6,7 @@ const ProjectsItems = ({item, link}) => {
       <img src={item.image} alt='' className='projects__img' />
       <h3 className='projects__title'>{item.title}</h3>
       <p className='projects__subtitle'>{item.subtitle}</p>
-      <a href={link} target='__blank' className='projects__button'>
+      <a href={link} target='__blank' rel='noopener noreferrer' className='projects__button'>
         View <i className='bx bx-right-arrow-alt projects__button-icon'></i>
       </a>
     </div>
