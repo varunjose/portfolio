cad573ff8aac9e7cefc070fe84b92d5f669c82ba
vmadanu <67040254+varunjose@users.noreply.github.com>
2026-08-11 14:47:15 -0400

    Normalize LinkedIn URL

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Social = () => {
   return (
     <div className='home__social'>
-      <a href='https://www.linkedin.com/in/varun-jose-madan' className='home__social-icon' target='_blank' rel='noreferrer'>
+      <a href='https://www.linkedin.com/in/varun-jose-madan/' className='home__social-icon' target='_blank' rel='noreferrer'>
         <i className="uil uil-linkedin-alt"></i>
       </a>
 
